718dd7360aa2f4116030c18df768a90bb5295da3
Лоник <l0nik@mail.ru>
2022-11-20 16:44:02 +0300

    Creating project

---
+++ app/src/main/java/com/example/waylists/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.waylists
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.waylists.ui.theme.WayListsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WayListsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                ) {
+                    MainMenu()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainMenu() {
+    Column(){
+        MenuItem("Путевые листы")
+        MenuItem("Установить остатки")
+        MenuItem("Топливные коэффициенты")
+    }
+}
+
+@Composable
+fun MenuItem(name: String) {
+    val context = LocalContext.current
+    Button(modifier = Modifier.fillMaxWidth(0.75f)
+        .padding(5.dp),
+        onClick = {
+            val intent = Intent(context, WayListHistoryActivity::class.java)
+            context.startActivity(intent)
+        },
+    ){
+        Text(text = "$name")
+    }
+}
